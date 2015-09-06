1998e1e082dfbbe74f8dc24c680e4b407125ce2a
3LOK <yoava@wix.com>
2015-09-06 18:14:50 +0300

    Added other languages

--- src/locales.js
+++ src/locales.js
@@ -1,4 +1,20 @@
 module.exports = {
     "en_US" : require("../resources/en_US.json"),
     "he_IL" : require("../resources/he_IL.json"),
+    "ar_AE" : require("../resources/ar_AE.json"),
+    "da_DK" : require("../resources/da_DK.json"),
+    "de_DE" : require("../resources/de_DE.json"),
+    "en_GB" : require("../resources/en_GB.json"),
+    "es_ES" : require("../resources/es_ES.json"),
+    "fr_FR" : require("../resources/fr_FR.json"),
+    "it_IT" : require("../resources/it_IT.json"),
+    "ja_JP" : require("../resources/ja_JP.json"),
+    "ko_KR" : require("../resources/ko_KR.json"),
+    "nb_NO" : require("../resources/nb_NO.json"),
+    "nl_NL" : require("../resources/nl_NL.json"),
+    "pl_PL" : require("../resources/pl_PL.json"),
+    "pt_BR" : require("../resources/pt_BR.json"),
+    "ru_RU" : require("../resources/ru_RU.json"),
+    "sv_SE" : require("../resources/sv_SE.json"),
+    "tr_TR" : require("../resources/tr_TR.json")
 };
