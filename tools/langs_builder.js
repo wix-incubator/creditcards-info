50ea90cbe2f021f920fc2670eaf75813053087c9
guybe <guybe@wix.com>
2015-12-20 17:32:12 +0200

    Using restaurants-i18n-builder to generate locale files

--- tools/langs_builder.js
+++ tools/langs_builder.js
@@ -1,4 +1,4 @@
-var recursive = require('recursive-readdir');
+var GitToI18NGenerator = require('restaurants-i18n-builder').GitToI18NGenerator;
 var fs_extra = require('fs-extra');
 var gift = require("gift");
 var path = require('path');
@@ -24,50 +24,32 @@ Q().then(function() {
 
     return deferred.promise;
 }).then(function(repo) {
-    var deferred = Q.defer();
-    var tmpDir = path.join(BASE_DIR, "src", "main", "resources", "creditcards");
-
+    // Generate all locales and corresponding tokens in an object
+    return GitToI18NGenerator.default.generate({gitProjectPath : repo.path});
+}).then(function(results) {
+    // Write each locale and its tokens to its own file
     var tokens = {};
+    _.each(results, function(data, locale) {
+        tokens[locale] = tokens[locale] || {};
+        _.each(data.tokens, function(val, key) {
+            var parts = key.split("_");
+            var obj = tokens[locale];
+            key = parts.pop();
 
-    recursive(tmpDir, function (err, files) {
-        if (err) {
-            console.log("Error reading directory [" + tmpDir + "]", err);
-            deferred.reject(err);
-            return;
-        }
-        _.each(files, function(file) {
-            // All files are in the format of networks_en-US.json
-            var locale = file.substr(-10, 5);
-            tokens[locale] = tokens[locale] || {};
+            _.each(parts, function(part) {
+                obj[part] = obj[part] || {};
+                obj = obj[part];
+            })
 
-            var data = fs.readFileSync(file, 'utf8');
-            var json = JSON.parse(data.toString('utf8'));
-
-            _.each(json, function(val, key) {
-                var parts = key.split("_");
-                var obj = tokens[locale];
-                key = parts.pop();
-
-                _.each(parts, function(part) {
-                    obj[part] = obj[part] || {};
-                    obj = obj[part];
-                })
-
-                obj[key] = val;
-            });
+            obj[key] = val;
         });
-        deferred.resolve(tokens);
-    });
-    return deferred.promise;
-}).then(function(tokens) {
-    _.each(tokens, function(token, locale) {
-        fs.writeFileSync(path.join(__dirname, "..", "resources", locale.replace("-", "_") + ".json"), JSON.stringify(token, null, 4), 'utf8');
+        fs.writeFileSync(path.join(__dirname, "..", "resources", locale + ".json"), JSON.stringify(tokens[locale], null, 4), 'utf8');
     });
     return tokens;
 }).then(function(tokens) {
+    // Generate locales.js which holds all of the locale files
     var fileContent = 'module.exports = {\n';
-    _.each(tokens, function(val, key) {
-        var locale = key.replace("-", "_");
+    _.each(_.keys(tokens), function(locale) {
         fileContent += '    "' + locale + '" : require("../resources/' + locale + '.json"),\n';
     });
     fileContent += '};\n';
