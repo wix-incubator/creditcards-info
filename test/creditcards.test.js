89d3cd186fbe51e1b7c01ae183177228500d15f4
Danny Leshem <dleshem@gmail.com>
2016-02-01 12:28:43 +0200

    Arrow functions.

--- test/creditcards.test.js
+++ test/creditcards.test.js
@@ -3,22 +3,22 @@ var _           = require("lodash");
 var creditcards = require("../src/Creditcards.js");
 
 
-describe("English locale tests (en_US)", function() {
-    it ('returns "Visa" for "visa" (network name)', function() {
+describe("English locale tests (en_US)", () => {
+    it ('returns "Visa" for "visa" (network name)', () => {
     	expect(creditcards.getCreditcardName("en_US", "visa")).to.equal("Visa");
     });
 
-    it ('returns "Expiry Date" for "expiration" (field name)', function() {
+    it ('returns "Expiry Date" for "expiration" (field name)', () => {
     	expect(creditcards.getFieldName("en_US", "expiration")).to.equal("Expiry Date");
     });
 });
 
-describe("Invalid locale tests (xx_XX)", function() {
-    it ('returns empty string for "visa" (network name)', function() {
+describe("Invalid locale tests (xx_XX)", () => {
+    it ('returns empty string for "visa" (network name)', () => {
     	expect(creditcards.getCreditcardName("xx_XX", "visa")).to.equal("");
     });
 
-    it ('returns empty string for "expiration" (field name)', function() {
+    it ('returns empty string for "expiration" (field name)', () => {
     	expect(creditcards.getFieldName("xx_XX", "expiration")).to.equal("");
     });
 });
